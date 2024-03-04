687c854d54f1d5a39c2a42b22d9735f66de314d8
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-03-04 01:45:54 +0000

    [Add]バリデーションでのエラーメッセージ表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,6 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      @book = Book.new
       @books = Book.all
       render :index
     end
@@ -25,12 +24,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      render:index
+      render :edit
     end
   end
 
